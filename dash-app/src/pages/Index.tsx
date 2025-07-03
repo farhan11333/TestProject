025df37c55bdf8a613c1592b92420f6c86c1b70c
Farhan <49912157+farhan11333@users.noreply.github.com>
2025-07-03 10:47:20 +0500

    Orders listing

--- dash-app/src/pages/Index.tsx
+++ dash-app/src/pages/Index.tsx
@@ -4,7 +4,7 @@ import { AppSidebar } from "../components/AppSidebar";
 import { DashboardHeader } from "../components/DashboardHeader";
 import { KPICards } from "../components/KPICards";
 import { ChartsSection } from "../components/ChartsSection";
-
+import { RecentOrders } from "../components/RecentOrders";
 
 const Index = () => {
   return (
@@ -16,7 +16,7 @@ const Index = () => {
           <main className="flex-1 p-4 md:p-6 space-y-6">
             <KPICards />
             <ChartsSection />
-           
+            <RecentOrders />
           </main>
         </div>
       </div>
